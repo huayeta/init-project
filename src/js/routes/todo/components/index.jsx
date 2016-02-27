40f2a462200e7afedfb770e211e111bb30f075f3
zh <zh@yingshangkeji.com>
2016-02-27 10:10:17 +0800

    add connect

--- src/js/routes/todo/components/index.jsx
+++ src/js/routes/todo/components/index.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
+import {connect} from 'react-redux';
+import {addTodo} from '../../../actions/actions';
 
 class TodoApp extends React.Component {
     constructor() {
         super();
     }
     render(){
+        const {dispatch,todos} =this.props;
         return(
             <div>
                 <AddTodo onAddClick={text=>{
-                        console.log('AddTodo',text);
+                        dispatch(addTodo(text));
                     }} />
                 <TodoList
-                    todos={[
-                        {text:'user'},
-                        {text:'learn'},
-                    ]} />
+                    todos={todos} />
             </div>
         )
     }
 }
 
-module.exports=TodoApp;
+module.exports=connect((state)=>{
+    return state;
+})(TodoApp);
