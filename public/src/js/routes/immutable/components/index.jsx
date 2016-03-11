0549e87ee6d988dbd29d65539a59613637ceab4e
zh <zh@yingshangkeji.com>
2016-03-11 16:24:32 +0800

    async

--- public/src/js/routes/immutable/components/index.jsx
+++ public/src/js/routes/immutable/components/index.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import 'babel-polyfill';
 import Immutable from 'immutable';
 
-let map1=Immutable.Map({a:1,b:2,c:3});
-// console.log(map1.get('b'));
-// let map2=map1.set('b',333);
-// console.log(map1.get('b'));
-// console.log(map2.get('b'));
+function timeout(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-let list1=Immutable.List.of(1,2,3);
-console.log(list1);
-console.log(list1.get(0));
+async function asyncPrint(value, ms) {
+  await timeout(ms);
+  console.log(value)
+}
+//
+asyncPrint('hello world', 50);
+
+// let map1=Immutable.Map({a:1,b:2,c:3});
+// // console.log(map1.get('b'));
+// // let map2=map1.set('b',333);
+// // console.log(map1.get('b'));
+// // console.log(map2.get('b'));
+//
+// let list1=Immutable.List.of(1,2,3);
+// console.log(list1);
+// console.log(list1.get(0));
 
 class ImmutableApp extends React.Component {
     constructor() {
